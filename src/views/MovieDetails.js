8c7dc898c165bfdab0bfb650b88992bc49419522
Vladislav Churilov <88vvch88@gmail.com>
2021-05-03 12:40:00 +0300

    fixed

--- src/views/MovieDetails.js
+++ src/views/MovieDetails.js
@@ -1,88 +1,122 @@
 import { Link, Route } from 'react-router-dom';
 // import PropTypes from 'prop-types';
 import axios from 'axios';
-import  { Component, Suspense, lazy } from 'react';
+import { Component, Suspense, lazy } from 'react';
 import styles from './MovieDetails.module.css';
 import routes from '../routes';
 
 const URL = 'https://api.themoviedb.org/3/movie/';
 const KEY = '300595d929d6a6ebde3ac2741883f41c';
 const Cast = lazy(() => import('./Cast' /* webpackChunkName: "Cast" */));
-const Reviews = lazy(() => import('./Reviews' /* webpackChunkName: "Reviews" */));
+const Reviews = lazy(() =>
+  import('./Reviews' /* webpackChunkName: "Reviews" */),
+);
 
 export default class MovieDetails extends Component {
-    // static defaultProps = {
-    //     defurl: 'https://cdn.pixabay.com/photo/2017/07/30/20/37/woman-2555564_960_720.jpg'
-    // }
-    // static propTypes = {
-    // }
-    state = {
-        film: [],
-        genres: [],
-        cast: [],        
-        reviews: []
-    }
+  // static defaultProps = {
+  //     defurl: 'https://cdn.pixabay.com/photo/2017/07/30/20/37/woman-2555564_960_720.jpg'
+  // }
+  // static propTypes = {
+  // }
+  state = {
+    film: [],
+    genres: [],
+    cast: [],
+    reviews: [],
+  };
 
-    async componentDidMount() {
-        const {movieId} = this.props.match.params;
-        const response = await axios.get(`${URL}${movieId}?api_key=${KEY}&language=en-US`);
-        const responseCast = await axios.get(`${URL}${movieId}/credits?api_key=${KEY}&language=en-US`);
-        const responseReviews = await axios.get(`${URL}${movieId}/reviews?api_key=${KEY}&language=en-US`);
-        
-        return this.setState({
-            film: response.data,
-            genres: response.data.genres,
-            cast: responseCast.data.cast,            
-            reviews: responseReviews.data
-        });
-    }
-    handleGoBack = () => {
-        const { location, history } = this.props;
-        history.push(location?.state?.from || routes.movies);
+  async componentDidMount() {
+    const { movieId } = this.props.match.params;
+    const response = await axios.get(
+      `${URL}${movieId}?api_key=${KEY}&language=en-US`,
+    );
+    const responseCast = await axios.get(
+      `${URL}${movieId}/credits?api_key=${KEY}&language=en-US`,
+    );
+    const responseReviews = await axios.get(
+      `${URL}${movieId}/reviews?api_key=${KEY}&language=en-US`,
+    );
 
-    }
-    
-    render() {
-        const { film, genres, cast, reviews } = this.state;
-        const poster = `https://image.tmdb.org/t/p/w500/${film.poster_path}`;
-        
-        return (
-            <>
-            <button className={styles.goBack} type="button" onClick={this.handleGoBack} > Back </button>
-            <div className={styles.MovieCard} >                    
-                <img src={poster} 
-                width="300" 
-                height="300" 
-                alt="Poster"
-                className={styles.poster} />
-                <div className={styles.filmConteiner}>
-                <h2 className={styles.title}>{film.title}</h2> 
-                <h3 className={styles.title}> Rating: {film.vote_average}</h3>                                
-                <ul className={styles.genresList}> Genres: {genres.map(item => (
-                   <li className={styles.genresItem} key={item.id}> {item.name}</li>
-                    ))}              
-                </ul>                
-                <span>{film.overview}</span>
-            </div>
-            </div>
-                <div className={styles.inform} >
-                    <p className={styles.paragr} >Additional information</p>
-                    <Link className={styles.informLink} to={`${this.props.match.url}/cast`}  >Cast</Link>
-                    <Link className={styles.informLink} to={`${this.props.match.url}/reviews`}  >Reviews</Link>
-                </div>
-                <Suspense fallback={<h1>Load...</h1>} >
+    return this.setState({
+      film: response.data,
+      genres: response.data.genres,
+      cast: responseCast.data.cast,
+      reviews: responseReviews.data,
+    });
+  }
+  handleGoBack = () => {
+    const { location, history } = this.props;
+    history.push(location?.state?.from || routes.movies);
+  };
 
-                <Route path={`${this.props.match.path}/cast`}
-                    render={props => (
-                         cast && <Cast {...props} cast={cast} /> )} />
-                        {/* return cast? <Cast {...props} cast={cast} /> : null}} /> */}
-                <Route path={`${this.props.match.path}/reviews`}
-                    render={props => (
-                        reviews && <Reviews {...props} reviews={reviews} />)} />
-                        {/* return reviews? <Reviews {...props} reviews={reviews} />: null}} /> */}
-                </Suspense>
-            </>
-        )
-    }
+  render() {
+    const { film, genres, cast, reviews } = this.state;
+    const poster = `https://image.tmdb.org/t/p/w500/${film.poster_path}`;
+
+    return (
+      <>
+        <button
+          className={styles.goBack}
+          type="button"
+          onClick={this.handleGoBack}
+        >
+          {' '}
+          Back{' '}
+        </button>
+        <div className={styles.MovieCard}>
+          <img
+            src={poster}
+            width="300"
+            height="300"
+            alt="Poster"
+            className={styles.poster}
+          />
+          <div className={styles.filmConteiner}>
+            <h2 className={styles.title}>{film.title}</h2>
+            <h3 className={styles.title}> Rating: {film.vote_average}</h3>
+            <ul className={styles.genresList}>
+              {' '}
+              Genres:{' '}
+              {genres.map(item => (
+                <li className={styles.genresItem} key={item.id}>
+                  {' '}
+                  {item.name}
+                </li>
+              ))}
+            </ul>
+            <span>{film.overview}</span>
+          </div>
+        </div>
+        <div className={styles.inform}>
+          <p className={styles.paragr}>Additional information</p>
+          <Link
+            className={styles.informLink}
+            to={`${this.props.match.url}/cast`}
+          >
+            Cast
+          </Link>
+          <Link
+            className={styles.informLink}
+            to={`${this.props.match.url}/reviews`}
+          >
+            Reviews
+          </Link>
+        </div>
+        <Suspense fallback={<h1>Load...</h1>}>
+          <Route
+            path={`${this.props.match.path}/cast`}
+            render={props => cast && <Cast {...props} cast={cast} />}
+          />
+          {/* return cast? <Cast {...props} cast={cast} /> : null}} /> */}
+          <Route
+            path={`${this.props.match.path}/reviews`}
+            render={props =>
+              reviews && <Reviews {...props} reviews={reviews} />
+            }
+          />
+          {/* return reviews? <Reviews {...props} reviews={reviews} />: null}} /> */}
+        </Suspense>
+      </>
+    );
+  }
 }
-
--- src/views/Movies.js
+++ src/views/Movies.js
@@ -1,4 +1,4 @@
-import  { Component } from 'react';
+import { Component } from 'react';
 import { Link, withRouter } from 'react-router-dom';
 import axios from 'axios';
 import styles from './Movies.module.css';
@@ -6,59 +6,77 @@ import styles from './Movies.module.css';
 const URL = 'https://api.themoviedb.org/3/search/movie';
 const KEY = '300595d929d6a6ebde3ac2741883f41c';
 
- class Movies extends Component {   
-    state={
-        query: '',
-        results: [],        
+class Movies extends Component {
+  state = {
+    query: '',
+    results: [],
+  };
+  componentDidMount() {
+    const results = localStorage.getItem('results');
+    const parsedResults = JSON.parse(results);
+    if (parsedResults) {
+      this.setState({ results: parsedResults });
     }
-    
-    async fetchFilm  () {
-        const response = await axios.get(`${URL}?api_key=${KEY}&query=${this.state.query}`);        
-        return this.setState({results: response.data.results})
-         
+  }
+  componentDidUpdate(prevProps, prevState) {
+    if (this.state.results !== prevState.results) {
+      localStorage.setItem('results', JSON.stringify(this.state.results));
     }
+  }
+  async fetchFilm() {
+    const response = await axios.get(
+      `${URL}?api_key=${KEY}&query=${this.state.query}`,
+    );
+    return this.setState({ results: response.data.results });
+  }
 
-    handleChange = e => {
-        this.setState({query: e.currentTarget.value});
-      }
-     handleSubmit = e => {
-        e.preventDefault();        
-         this.fetchFilm();        
-        this.setState({query:''})
-    }
+  handleChange = e => {
+    this.setState({ query: e.currentTarget.value });
+  };
+  handleSubmit = e => {
+    e.preventDefault();
+    this.fetchFilm();
+    this.setState({ query: '' });
+  };
 
-    render() {
-        const {results} = this.state;
-        
-        return (
-            <div className={styles.movies}>
-        <form className={styles.SearchForm} onSubmit = {this.handleSubmit} >
-            <input
+  render() {
+    const { results } = this.state;
+
+    return (
+      <div className={styles.movies}>
+        <form className={styles.SearchForm} onSubmit={this.handleSubmit}>
+          <input
             className={styles.SearchFormInput}
             type="text"
-            autoComplete="off" 
+            autoComplete="off"
             value={this.state.query}
-            onChange={this.handleChange}     
+            onChange={this.handleChange}
             autoFocus
             placeholder="Search movies"
-            />
-            <button type="submit"  className={styles.SearchFormButton}>Search</button>
+          />
+          <button type="submit" className={styles.SearchFormButton}>
+            Search
+          </button>
         </form>
         <ul className={styles.filmList}>
-            {results.map(movie => (
-            <li key={movie.id} className={styles.item}>                    
-                <Link to={{
-                    pathname: `${this.props.match.url}/${movie.id}`,
-                    state: {
-                        from: this.props.location,
-                    }
-                }}>{movie.title}</Link>
-            </li>                    
-            ))}
-        </ul>            
-        </div>
-        )
-    }
+          {results.map(movie => (
+            <li key={movie.id} className={styles.item}>
+              <Link
+                to={{
+                  pathname: `${this.props.match.url}/${movie.id}`,
+                  state: {
+                    from: this.props.location,
+                  },
+                }}
+              >
+                {movie.title}
+              </Link>
+            </li>
+          ))}
+        </ul>
+      </div>
+    );
+  }
 }
 
 export default withRouter(Movies);
